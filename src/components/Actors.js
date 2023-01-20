6a9e7ddd0513435e17973a244703db39dc887694
rose534 <wambuirose534@gmail.com>
2023-01-20 17:11:29 +0300

    lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1> Actors Page </h1>
+    <div>
+    {actors.map((actors) => (
+          <div key={actors.name}>
+            <p>Name: {actors.name}</p>
+            <ul>
+              <p>Movies:</p>
+              {actors.movies.map((actor) => (
+                <li key={actor}>{actor}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    
+  </div>;
 }
 
 export default Actors;
